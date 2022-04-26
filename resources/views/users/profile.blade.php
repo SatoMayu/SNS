2332e62fe71a60d55fc516c17e0bfa45b71018f7
SatoMayu <hanon1742@gmail.com>
2022-04-26 12:36:41 +0900

    ログインユーザーのプロフィール編集フォームの設置

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -2,7 +2,18 @@
 
 @section('content')
 
-
+<p>アイコン画像</p>
+<form action=""></form>
+<p>user name</p>
+<form action=""></form>
+<p>mail address</p>
+<form action=""></form>
+<p>password</p>
+<form action=""></form>
+<p>bio</p>
+<form action=""></form>
+<p>icon image</p>
+<form action=""></form>
 
 
 
