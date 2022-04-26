3db1b46a5a4250ec12b40a467edb46c5af1d5b8b
SatoMayu <hanon1742@gmail.com>
2022-04-26 15:57:47 +0900

    プロフィール編集入力フォームの設置

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -2,19 +2,36 @@
 
 @section('content')
 
-<p>アイコン画像</p>
-<form action=""></form>
-<p>user name</p>
-<form action=""></form>
-<p>mail address</p>
-<form action=""></form>
-<p>password</p>
-<form action=""></form>
-<p>bio</p>
-<form action=""></form>
-<p>icon image</p>
-<form action=""></form>
+<p>{{$user->images}}</p>
 
+<form action="/profile/update" method="POST">
+  @csrf
+  <p>
+    user name <input type="text" name="newName" placeholder={{$user->username}}>
+  </p>
 
+  <p>
+    mail address <input type="address" name="newMail" placeholder={{$user->mail}}>
+  </p>
+
+  <p>
+    password <input type="password" name="newPass" placeholder={{$user->password}}>
+  </p>
+
+  <p>
+    password comfirm <input type="password" name="newPass" placeholder={{$user->password}}>
+  </p>
+
+  <p>
+    bio <input type="text" name="newBio" placeholder={{$user->bio}}>
+  </p>
+
+  <p>
+    icon image <input type="file" name="newImage">
+  </p>
+
+  <button type="submit" alt="更新ボタン">更新する</button>
+
+</form>
 
 @endsection
