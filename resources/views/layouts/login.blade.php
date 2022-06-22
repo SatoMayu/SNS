359c6953ae532c644e965e2b5a66e8ed113ab0ed
SatoMayu <hanon1742@gmail.com>
2022-06-22 11:42:19 +0900

    アコーディオンメニュー追加

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -29,14 +29,20 @@
             <div id="">
                 <div id="">
 
-                    <p>{{Auth::user()->username}}さん<img src="{{asset('storage'.Auth::user()->images)}}"></p>
-                <div>
-                <ul>
-                    <li><a href="/top">ホーム</a></li>
-                    <li><a href="/profile">プロフィール</a></li>
-                    <li><a href="/logout">ログアウト</a></li>
-                </ul>
-            </div>
+                    <p>{{Auth::user()->username}}さん</p>
+
+                    <!-- アコーディオンメニュー開始 -->
+                    <button type="button" class="menu-btn">
+                        <span class="inn"></span>
+                    </button>
+                    <nav class="menu">
+                        <ul>
+                            <li><a href="/top">ホーム</a></li>
+                            <li><a href="/profile">プロフィール</a></li>
+                            <li><a href="/logout">ログアウト</a></li>
+                        </ul>
+                    </nav>
+                    <img src="{{asset('storage'.Auth::user()->images)}}">
         </div>
     </header>
     <div id="row">
@@ -62,11 +68,15 @@
     </div>
     <footer>
     </footer>
+
+
+
     <!-- <script src="js/script.js"></script> -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
     <script src="{{asset('js/script.js')}}"></script>
+
 
 </body>
 </html>
