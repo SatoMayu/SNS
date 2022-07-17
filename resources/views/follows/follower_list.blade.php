31d08113c75a027f330d80f2323a25099f3410b7
SatoMayu <hanon1742@gmail.com>
2022-07-18 07:34:41 +0900

    follower_list　classづけ

--- resources/views/follows/follower_list.blade.php
+++ resources/views/follows/follower_list.blade.php
@@ -1,21 +1,33 @@
 @extends('layouts.login')
 @section('content')
+<div class = "first_content">
+  <div class="first_content_wrap">
+    <h1>Follower List</h1>
+    @foreach ($followed_users as $followed_user)
+    <div follow-list_icons>
+      <a href="/users/{{$followed_user->id}}/users_profile">
+        <img src="{{asset('storage/'.$followed_user->images)}}">
+      </a>
+    </div>
+    @endforeach
+</div>
+</div>
 
-<h1>Follower List</h1>
-@foreach ($followed_users as $followed_user)
-  <a href="/users/{{$followed_user->id}}/users_profile">
-    <img src="{{asset('storage/'.$followed_user->images)}}">
-  </a>
-  <p>{{$followed_user->username}}</p>
-
-@endforeach
-
-@foreach($posts as $post)
-  <a href="/users/{{$post->user->id}}/users_profile">
-    <img src="{{asset('storage/'.$post->images)}}"></a>
-  <p>名前：{{$post->user->username}}</p>
-  <p>投稿内容：{{$post->post}}</p>
-
-@endforeach
-
+<div class = "second_content">
+  @foreach($posts as $post)
+    <div class = "tweet-list">
+      <div class = "tweet-list_left">
+        <a href="/users/{{$post->user->id}}/users_profile">
+        <img src="{{asset('storage/'.$post->user->images)}}"></a>
+          <div class ="tweet-list_left_inner">
+            <p>名前：{{$post->user->username}}</p>
+            <p>投稿内容：{{$post->post}}</p>
+          </div>
+      </div>
+      <div class ="tweet-list_right">
+        <p>{{$post->created_at}}</p>
+      </div>
+    </div>
+  @endforeach
+</div>
 @endsection
