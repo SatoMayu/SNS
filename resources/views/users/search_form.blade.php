7c5d97c5137d7ed2f1b22a3f55ff7ac9690c5666
SatoMayu <hanon1742@gmail.com>
2022-07-21 21:26:03 +0900

    検索ページのhtml調整

--- resources/views/users/search_form.blade.php
+++ resources/views/users/search_form.blade.php
@@ -1,48 +1,60 @@
 @extends('layouts.login')
 
 @section('content')
+<div class = "first_content">
+  <div class="first_content_wrap">
+    <div>
+      <form action="http://127.0.0.1:8000/search" method="POST">
+        @csrf
+        <div class = "search-form_inner_left">
+          <p><input type="text" name="keyword_name" placeholder="ユーザー名"></p>
+          <P><input type="submit" value="検索"></p>
+        </div>
+      </form>
+    </div>
+    <div class = "search-form_inner_right">
+      @if(!empty($keyword_name))
+      <p>検索ワード：{{$keyword_name}}</p>
+      @endif
+    </div>
+  </div>
+</div>
 
-<form action="http://127.0.0.1:8000/search" method="POST">
-  @csrf
-  <p><input type="text" name="keyword_name" placeholder="ユーザー名"></p>
-  <P><input type="submit" value="検索"></p>
-</form>
-
-@if(!empty($keyword_name))
-  <p>検索ワード：{{$keyword_name}}</p>
-@endif
-
-
-
-<table>
+<div class = "second_content">
   @foreach ($users as $users)
-  @if($users->id != Auth::id())
-    <tr>
-      <td><img src="{{asset('storage/'.$users->images)}}"></td>
-      <td>{{$users->username}}</td>
-      <td>
-        @if (auth()->user()->isFollowing($users->id))
+    <div class = "result-list">
+      @if($users->id != Auth::id())
+        <div class = "tweet-list_left">
+          <td><img src="{{asset('storage/'.$users->images)}}"></td>
+          <div class ="tweet-list_left_inner">
+            <td>{{$users->username}}</td>
+          </div>
+        </div>
+        <div class ="tweet-list_right">
+          <td>
+            @if (auth()->user()->isFollowing($users->id))
 
-        <form action="{{route('unfollow',['id'=>$users->id])}}" method="POST">
-          {{csrf_field()}}
-          {{method_field('DELETE')}}
+            <form action="{{route('unfollow',['id'=>$users->id])}}" method="POST">
+              {{csrf_field()}}
+              {{method_field('DELETE')}}
 
-          <button type="submit" >フォロー解除</button>
-        </form>
+              <button type="submit" class = "btn btn-danger remove-btn">フォロー解除</button>
+            </form>
 
-        @else
+            @else
 
-        <form action="{{route('follow',['id'=>$users->id])}}" method="POST">
-          {{csrf_field()}}
-          <button type="submit" >フォローする</button>
-        </form>
-        @endif
-      </td>
-    </tr>
-    @endif
+            <form action="{{route('follow',['id'=>$users->id])}}" method="POST">
+              {{csrf_field()}}
+              <button type="submit"  class = "btn btn-primary following-btn">フォローする</button>
+            </form>
+            @endif
+          </td>
+        </div>
+      @endif
+
+      </div>
     @endforeach
-</table>
-
+</div>
 
 
 
